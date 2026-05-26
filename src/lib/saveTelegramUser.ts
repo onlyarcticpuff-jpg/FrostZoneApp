237dae21b535efba561809d4bd2d9a97644fe639
onlyarcticpuff-jpg <onlyarcticpuff@gmail.com>
2026-05-26 19:16:40 +0200

    Update saveTelegramUser.ts

--- src/lib/saveTelegramUser.ts
+++ src/lib/saveTelegramUser.ts
@@ -1,9 +1,9 @@
 import { createClient } from "@supabase/supabase-js";
 
-const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL ?? "";
-const SUPABASE_ANON_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY ?? "";
+const VITE_SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL ?? "";
+const VITE_SUPABASE_ANON_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY ?? "";
 
-const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+const supabase = createClient(VITE_SUPABASE_URL, VITE_SUPABASE_ANON_KEY);
 
 export async function saveTelegramUser() {
   try {
